36e8177212d8f8b71f3d92666127f8ede2c961bd
Styvensoft <steveen.echeverri@gmail.com>
2025-03-17 21:11:48 -0500

    Initial setup basic

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
+import ContactTable from './components/ContactTable.vue'
+
+const titlePage = 'Agenda - Lista de contactos'
+</script>
+
+<template>
+  <div class="flex items-center flex-col py-5">
+    <h1>{{ titlePage }}</h1>
+    <Search />
+  </div>
+  <ContactTable />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ContactTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Id
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombres
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Número
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Correo
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                    <td class="px-6 py-4">
+                       <a href="#" class="py-2 px-4 bg-purple-900 rounded-full">Editar</a> 
+                    </td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+</template>
